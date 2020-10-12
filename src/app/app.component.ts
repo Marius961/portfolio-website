291c0ed310f9b46d5492a9f619ebe6dc05eeac27
Marius <marius.shiman@gmail.com>
2020-10-12 13:39:47 +0300

    Created base project structure

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pw-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  title = 'portfolio-website';
+}
