a41c6d6bc9d9c367262cf3b13c369926d127befc
Marius <marius.shiman@gmail.com>
2020-11-24 13:20:23 +0200

    Fixed bug with size of project images, removed unused section

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,8 +2,6 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
 import {HomeComponent} from './home.component';
-import {MyPrinciplesSectionComponent} from './components/my-principles-section/my-principles-section.component';
-import {PrincipleCardComponent} from './components/my-principles-section/principle-card/principle-card.component';
 import {TechnologyItemComponent} from './components/section-about/technology-item/technology-item.component';
 import {ProjectCardComponent} from './components/section-portfolio/project-card/project-card.component';
 import {SectionTitleComponent} from './components/section-title/section-title.component';
@@ -16,7 +14,7 @@ import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
-  declarations: [HomeComponent, MyPrinciplesSectionComponent, PrincipleCardComponent, TechnologyItemComponent, ProjectCardComponent, SectionTitleComponent, SectionContactComponent, SectionMainComponent, SectionAboutComponent, SectionPortfolioComponent, HomeNavComponent],
+  declarations: [HomeComponent, TechnologyItemComponent, ProjectCardComponent, SectionTitleComponent, SectionContactComponent, SectionMainComponent, SectionAboutComponent, SectionPortfolioComponent, HomeNavComponent],
   imports: [
     CommonModule,
     FormsModule,
