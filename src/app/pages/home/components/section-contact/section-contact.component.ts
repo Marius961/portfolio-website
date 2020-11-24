81e556e35db07676c04e1583868854bda015c671
Marius <marius.shiman@gmail.com>
2020-11-24 14:40:58 +0200

    Created footer, improved contact form

--- src/app/pages/home/components/section-contact/section-contact.component.ts
+++ src/app/pages/home/components/section-contact/section-contact.component.ts
@@ -15,6 +15,7 @@ export class SectionContactComponent implements OnInit {
   @ViewChild("form")
   private contactForm: NgForm;
 
+  public loading: boolean = false;
   public contactEmail = environment.contactEmail;
   public emailMessage: EmailMessage = new EmailMessage();
 
@@ -24,15 +25,21 @@ export class SectionContactComponent implements OnInit {
   }
 
   public sendEmail(): void {
+    this.loading = true;
+
     const onSuccess = () => {
       swal.fire("Thant you!", "Thank you for your message, I will try to answer it as soon as possible.", "success");
       this.resetForm();
+      this.loading = false;
     }
-    const onError = () => swal.fire(
-      "Sorry!",
-      `Something went wrong. Refresh the page and try again. <br><br> If you have any problems, please contact me at <a href='mailto:${this.contactEmail}'>this</a> email address.`,
-      "error"
-    );
+    const onError = () => {
+      swal.fire(
+        "Sorry!",
+        `Something went wrong. Refresh the page and try again. <br><br> If you have any problems, please contact me at <a href='mailto:${this.contactEmail}'>this</a> email address.`,
+        "error"
+      );
+      this.loading = false;
+    }
 
     this.emailService.sendEmail(this.emailMessage).subscribe(onSuccess, onError);
   }
